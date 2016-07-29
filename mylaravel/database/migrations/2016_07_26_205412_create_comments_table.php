dee3dbe983829959ebe36458b4173832268c032f
linyue <769353695@qq.com>
2016-07-29 22:52:42 +0800

    fix

--- mylaravel/database/migrations/2016_07_26_205412_create_comments_table.php
+++ mylaravel/database/migrations/2016_07_26_205412_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
         //
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+			$table->integer('lastid');
 			$table->string('name');
             $table->string('content');      	
             $table->timestamps();
